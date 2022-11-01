cd7cda40f5df753d30bcf2fe7830e82a042885a6
Anjay-2022 <anjay2013@gmail.com>
2022-11-01 20:32:24 +0530

    first commit to uc3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,8 +3,25 @@ package Assignment;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int ran=(int) (Math.random()*(3));
+		int WagePerHour=20;
+		int FullDayHour=8;
+		int PartTimeHour=4;
 
+		int EmployeeWage = 0;
+		if (ran == 0) {
+			System.out.println("employee is Absent");
+			EmployeeWage=0;
+		} else if (ran == 1) {
+			System.out.println("employee is Present Full Time");
+			EmployeeWage=WagePerHour*FullDayHour;
+		} else if (ran == 2) {
+			System.out.println("employee is Present Part Time");
+			EmployeeWage=WagePerHour*PartTimeHour;
+		}
+
+		System.out.println("EmployeeWage is  :"+EmployeeWage); 
 	}
 
 }
