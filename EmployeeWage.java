b135ecb928afa17568445f9832481726f0bd350c
Anjay-2022 <anjay2013@gmail.com>
2022-11-01 20:20:01 +0530

    first commit to uc1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,13 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int ran=(int) (Math.random()*(2));
+		System.out.println(ran);
+		if (ran == 0) {
+			System.out.println("employee is Absent");
+		} else if (ran == 1) {
+			System.out.println("employee is Present");
+		}
 
 	}
 
